f2eb81b5d59d708363c309271813dc4672cc632a
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-28 16:02:04 +0900

    Add comment hideable check

--- qa-hidden-restriction-overrides.php
+++ qa-hidden-restriction-overrides.php
@@ -1,11 +1,18 @@
 <?php
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+require_once QA_PLUGIN_DIR.'q2a-hidden-restriction/hidden-restriction.php';
 
 function qa_page_q_post_rules($post, $parentpost=null, $siblingposts=null, $childposts=null)
 {
 	$rules = qa_page_q_post_rules_base($post, $parentpost, $siblingposts, $childposts);
 	if ($post['basetype'] == 'C') {
-		error_log(serialize($post));
-		$rules['hideable'] = false;
+		if (hidden_restriction::passed_for_24_hours($post['created']) ||
+			!hidden_restriction::is_last_comment($post['postid'], $parentpost['postid'])) {
+			$rules['hideable'] = false;
+		}
 	}
 	return $rules;
 }

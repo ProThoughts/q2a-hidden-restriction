a21e1395845f0ed0569cacab1feb2a804dfe327f
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-28 10:38:42 +0900

    Add plugin file

---
+++ qa-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+	Plugin Name: Hidden Restriction
+	Plugin URI:
+	Plugin Description:  Comment and Question Nondisplay is restricted
+	Plugin Version: 1.0
+	Plugin Date: 2016-06-28
+	Plugin Author: 38qa.net
+	Plugin Author URI:
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/

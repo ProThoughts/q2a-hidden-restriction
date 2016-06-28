9028032d3df4e2b649d8865a1974ddfc3c4aa193
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-28 11:23:41 +0900

    Rename overrides file

--- qa-hidden-restriction-overrides.php
+++ qa-hidden-restriction-overrides.php
@@ -3,5 +3,9 @@
 function qa_page_q_post_rules($post, $parentpost=null, $siblingposts=null, $childposts=null)
 {
 	$rules = qa_page_q_post_rules_base($post, $parentpost, $siblingposts, $childposts);
+	if ($post['basetype'] == 'C') {
+		error_log(serialize($post));
+		$rules['hideable'] = false;
+	}
 	return $rules;
 }

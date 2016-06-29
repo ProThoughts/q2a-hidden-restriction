76367245e03f89c75fb369dff2ca6d73d1acaf1e
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-29 14:37:46 +0900

    Add condition for display button

--- qa-hidden-restriction-layer.php
+++ qa-hidden-restriction-layer.php
@@ -6,17 +6,20 @@ class qa_html_theme_layer extends qa_html_theme_base
 {
 	public function q_view_buttons($q_view)
 	{
-		if ($this->template == 'question') {
+		if ($this->template == 'question' &&
+			isset($q_view['form']['buttons']['hide']) &&
+			!empty($q_view['form']['buttons']['hide'])) {
+
 			$postid = $q_view['raw']['postid'];
 			$acount = $q_view['raw']['acount'];
 			$created = $q_view['raw']['created'];
 
 			if(hidden_restriction::passed_for_24_hours($created) ||
-			   $acount > 0) {
+				$acount > 0) {
 				$button = array( 'tags' => 'name="a_'.$postid .
 				 '" onclick="return false"',
-								 'label' => '非表示不可(詳細)',
-								 'popup' => '' );
+								 'label' => qa_lang_html('qa_hidden_restrictioin_lang/button_value'),
+								 'popup' => qa_lang_html('qa_hidden_restrictioin_lang/popup'));
 				$q_view['form']['buttons']['hide'] = $button;
 			}
 		}

e8c93621c7e1ba6dcd9660e33df5892b8dfed420
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-07 11:23:49 +0900

    Add Answer hideable condition

--- qa-hidden-restriction-overrides.php
+++ qa-hidden-restriction-overrides.php
@@ -8,9 +8,16 @@ require_once QA_PLUGIN_DIR.'q2a-hidden-restriction/hidden-restriction.php';
 function qa_page_q_post_rules($post, $parentpost=null, $siblingposts=null, $childposts=null)
 {
 	$rules = qa_page_q_post_rules_base($post, $parentpost, $siblingposts, $childposts);
-	if ($post['basetype'] == 'C') {
+	if ($post['basetype'] === 'C') {
 		if (hidden_restriction::passed_for_24_hours($post['created']) ||
 			!hidden_restriction::is_last_comment($post['postid'], $parentpost['postid'])) {
+			$rules['hideable'] = false;
+		}
+	} elseif ($post['basetype'] === 'A') {
+		if ( qa_get_logged_in_level() < QA_USER_LEVEL_ADMIN &&
+			(hidden_restriction::passed_for_24_hours($post['created']) ||
+			hidden_restriction::has_comment($post['postid']) ||
+			hidden_restriction::is_best_answer($post['postid']))) {
 			$rules['hideable'] = false;
 		}
 	}
